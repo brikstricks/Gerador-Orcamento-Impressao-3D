9876db9f720f4172a68adf204a5a1dad821d5bc1
brikstricks <pablocassani88@gmail.com>
2025-08-25 09:44:17 -0300

    Alinhamento

--- GUI_app2.py
+++ GUI_app2.py
@@ -289,7 +289,7 @@ class OrcamentoPrintai3D(QMainWindow):
         # Escolher onde salvar
         filename, _ = QFileDialog.getSaveFileName(
             self, "Salvar Orçamento", 
-            f"Orcamento_{projeto.replace(' ', '-')}_{datetime.now().strftime('%d%m%Y_%H%M')}.pdf",
+            f"Orcamento-{projeto.replace(' ', '-')}_{datetime.now().strftime('%d-%m-%Y_%H:%M')}.pdf",
             "Arquivos PDF (*.pdf)"
         )
         
